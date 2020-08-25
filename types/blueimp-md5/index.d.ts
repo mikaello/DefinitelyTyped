91c7d600324f4bf9960207c72588f42befb71925
mikaello <2505178+mikaello@users.noreply.github.com>
2020-08-25 08:56:36 +0200

    docs: add documentation to blueimp-md5

--- types/blueimp-md5/index.d.ts
+++ types/blueimp-md5/index.d.ts
@@ -3,5 +3,15 @@
 // Definitions by: Ray Martone <https://github.com/rmartone>, Mikael Kohlmyr <https://github.com/mkohlmyr>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
+/**
+ * Calculates MD5 value for a given string.
+ * If a key is provided, calculates the HMAC-MD5 value.
+ * Returns a Hex encoded string unless the raw argument is given.
+ *
+ * @param {string} value Input string
+ * @param {string} [key] HMAC key
+ * @param {boolean} [raw] Raw output switch
+ * @returns {string} MD5 output
+ */
 declare function md5(value: string, key?: string, raw?: boolean): string;
 export = md5;
